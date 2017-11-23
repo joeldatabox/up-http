2b57e584b628b67f383235100acff30ca5092352
joel.databox@gmail.com <joel.databox@gmail.com>
2017-11-22 22:00:24 -0200

    Adicionado algumas mascaras

--- src/app/utils/mask-util.ts
+++ src/app/utils/mask-util.ts
@@ -6,6 +6,10 @@ export class MaskUltil {
     return emailMask;
   }
 
+  public static get maskCnpj() {
+    return {mask: [/\d/, /\d/, ".", /\d/, /\d/, /\d/, ".", /\d/, /\d/, /\d/, "/", /\d/, /\d/, /\d/, /\d/, "-", /\d/, /\d/,]};
+  }
+
   public static get maskFone() {
     return {mask: ["(", /[1-9]/, /[1-9]/, ")", " ", /\d/, /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/, /\d/]};
   }
@@ -16,5 +20,9 @@ export class MaskUltil {
 
   public static get maskDate() {
     return {mask: [/\d/, /\d/, "/", /\d/, /\d/, "/", /\d/, /\d/, /\d/, /\d/], placeholderChar: " ", keepCharPositions: false, pipe: createAutoCorrectedDatePipe("dd/mm/yyyy")};
+  }
+
+  public static get maskMoney() {
+    return createNumberMask({prefix: "", thousandsSeparatorSymbol: ".", decimalSymbol: ",", allowDecimal: true, requireDecimal: true});
   }
 }
